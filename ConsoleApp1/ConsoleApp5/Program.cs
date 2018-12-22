e9dbbdf68318af566bd67b70e5719fe33dc54c57
Kiet Tran <17110165@student.hcmute.edu.vn>
2018-12-22 21:54:54 +0700

    ConsoleApp5
    Tao mới thay code console app 5 r

---
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = TaoDanhSach();
+            //Tham số a và trả về a có phải là số chẵn
+            //var even = list.Where(a => a % 2 == 0); //Kiểm tra số chẵn
+            var even = from a in list
+                       where a % 2 == 0
+                       select a;
+
+            foreach (var a in even)
+            {
+                Console.WriteLine(a);   //In những số là số chẵn
+            }
+            Console.WriteLine("Hello world ");
+        }
+        /// <summary>
+        /// Phương thức in ra danh sách từ 0 --> 10
+        /// </summary>
+        static IEnumerable<int> TaoDanhSach()
+        {
+            for (int i = 0; i <= 10; i++)
+                yield return i;
+        }
+    }
+}
